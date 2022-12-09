d89ce37ca1a5801f27f6f895822f60cadc219efb
Giveonaldo <giveonaldo@gmail.com>
2022-12-09 14:53:02 +0700

    add code V1.0

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
+using std::string;
+using std::cout;
+using std::cin;
+using std::ifstream;
+
+bool IsLoggedIn()
 {
-	return 0;
+	string username, password, un, pw;
+	cout << "Enter username: ";
+	cin >> username;
+	cout << "Enter password: ";
+	cin >> password;
+
+	ifstream read("data\\" + username + ".txt");
+	getline(read, un);
+	getline(read, pw);
+
+	if (un == username && pw == password)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
