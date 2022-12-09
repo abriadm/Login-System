40d22f79ad19fc5dcfd0549ecb8269d129f8b123
Giveonaldo <giveonaldo@gmail.com>
2022-12-09 15:14:40 +0700

    Code V1.1

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using std::string;
 using std::cout;
 using std::cin;
 using std::ifstream;
+using std::ofstream;
+using std::endl;
 
 bool IsLoggedIn()
 {
@@ -27,4 +29,46 @@ bool IsLoggedIn()
 	{
 		return false;
 	}
+}
+
+int main()
+{
+	string username, password;
+	int choice;
+	ofstream file;
+	bool status = IsLoggedIn();
+
+	cout << "1. Register" << endl;
+	cout << "2. Login" << endl;
+	cout << "Your Choice: ";
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		cout << "Create Username: ";
+		cin >> username;
+		cout << "Create Password: ";
+		cin >> password;
+
+		file.open("data\\" + username + ".txt");
+		file << username << endl;
+		file << password << endl;
+		file.close();
+		main();
+		break;
+	case 2:
+		if (!status)
+		{
+			cout << "Wrong!!!!!, who the Fuck are u!!!!!!" << endl;
+		}
+		else
+		{
+			cout << "Welcome Back!!!" << endl;
+		}
+		break;
+	default:
+		break;
+	}
+	return 0;
 }
